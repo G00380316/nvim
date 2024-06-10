4242af6b661529a8972b05fd4218ce090031dcd5
ENOCH ABIODUN <G00380316@atu.ie>
2024-06-10 16:41:30 +0100

    Changed the quit insert mode mapping in terminal again

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.api.nvim_set_keymap('t', '<C-q>', '<C-\\><C-n>', {noremap = true})
+vim.api.nvim_set_keymap('t', '<C-v>', '<C-\\><C-n>', {noremap = true})
 vim.o.shell = '"C:\\Program Files\\Git\\bin\\bash.exe"'
 vim.g.mapleader = " "
 
