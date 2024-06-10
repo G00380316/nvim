b9f2eedbba15ffd9ee51b1ca804ff2545913ebf4
ENOCH ABIODUN <G00380316@atu.ie>
2024-06-10 05:10:02 +0100

    Lazy_Neovim_Config

---
+++ init.lua
@@ -0,0 +1,32 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {}
+
+require("lazy").setup("plugins", opts)
+
+vim.keymap.set('n', '<C-e>', ':Neotree filesystem reveal left', {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"c","lua","vim","html","css","java","javascript","cpp","gitignore","php","python","xml","typescript","yaml","ssh_config","sql","csv","dockerfile","json","json5"},
+  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },
+})
---
+++ lua/plugins/material.lua
@@ -0,0 +1,8 @@
+return  {
+  "marko-cerovac/material.nvim",
+  name = "material",
+  priority = 1000,
+  config = function()
+  vim.cmd.colorscheme "material"
+  end
+}
