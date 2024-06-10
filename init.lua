0f90f3a92afee29cae0a7f47b8cb2674de890e8a
ENOCH ABIODUN <G00380316@atu.ie>
2024-06-10 15:01:09 +0100

    changed terminal to git bash

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.o.shell = '"C:\\Program Files\\Git\\bin\\bash.exe"'
 vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
