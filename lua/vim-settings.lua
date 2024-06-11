63c7dc4279d4260b6505fb307672075c231849fb
ENOCH ABIODUN <G00380316@atu.ie>
2024-06-12 00:23:58 +0100

    Added keymap to switch buffer(windows) in nvim

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -4,6 +4,8 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.api.nvim_set_keymap('t', '<C-v>', '<C-\\><C-n>', {noremap = true})
+vim.api.nvim_set_keymap({'n','v'}, '<leader>s', ':b#<CR>', { noremap = true, silent = true })
+
 vim.o.shell = '"C:\\Program Files\\Git\\bin\\bash.exe"'
 
 vim.g.mapleader = " "
