21e597232780d274e237725f9fcaff75dbb66fc0
ENOCH ABIODUN <G00380316@atu.ie>
2024-06-12 15:54:48 +0100

    Got autoDetect for LSP fully working I believe

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,14 +18,9 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
 
-      vim.keymap.set({'n', 'v'},'I', vim.lsp.buf.hover,{}) -- This gives you information on the function or keyword (shift and i) 
+      vim.keymap.set({'n', 'v'},'I', vim.lsp.buf.hover,{}) -- This gives you information on the function or keyword (shift and i)
       vim.keymap.set({'n', 'v'},'D', vim.lsp.buf.definition,{}) -- This takes you to the module or place where a function is usually defined (shift and d)
       vim.keymap.set({'n','v'}, '<leader>a', vim.lsp.buf.code_action,{}) -- To use code actions (space and a) this allows you to see warnings given by the LSP
-
-
-      lspconfig.lua_ls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.tsserver.setup({})
 
       -- Function to get the root directory for LSP servers
       local function get_root_dir()
@@ -37,34 +32,16 @@ return {
         return vim.fn.fnamemodify(fname, ":p:h")
       end
 
-      -- Function to re-setup the LSP servers
-      local function setup_servers()
-        local new_root = get_root_dir()
+      lspconfig.lua_ls.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.tsserver.setup({})
 
-      -- List of LSP servers to setup
-      local servers = {
-        lua_ls = {},
-        pyright = {},
-        tsserver = {}
-        -- Add more servers as needed
-      }
-
-      for server, config in pairs(servers) do
-        config.root_dir = get_root_dir
-        lspconfig[server].setup(config)
+      -- Autocommand to re-setup the LSP servers on BufEnter event
+      vim.api.nvim_create_autocmd("BufEnter", {
+      pattern = "*",
+      callback = function()
         end
-      end
-
-    -- Initial setup of LSP servers
-    setup_servers()
-
-    -- Autocommand to re-setup the LSP servers on BufEnter event
-    vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = "*",
-    callback = function()
-      setup_servers()
-      end
-    })
+      })
     end
   }
 
