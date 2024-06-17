ca21e59a05a9bd640a50287304b8fd9bc51a912e
ENOCH ABIODUN <G00380316@atu.ie>
2024-06-17 14:02:17 +0100

    add usefull settings

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -2,10 +2,13 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set cursorline")
+vim.cmd("set background=dark")
 
 vim.wo.number = true
 
 vim.opt.swapfile = false
+vim.opt.updatetime = 100
 
 vim.api.nvim_set_keymap('t', '<C-v>', '<C-\\><C-n>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-s>', '<C-w>w', { noremap = true, silent = true }) -- Remaps the switch window in nvim to (ctrl and s) 
