86031d471bc5dd20605f2c53f6a7f09846df9b5f
ENOCH ABIODUN <G00380316@atu.ie>
2024-07-07 20:29:15 +0100

    Neo tree change

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,16 +1,16 @@
 return {
-   "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
   config = function()
-    require('neo-tree').setup{}
-    vim.keymap.set('n', '<C-e>', ':Neotree filesystem reveal left<CR>', {}) -- <CR> immitates enter so we don't have to press enter after Ctrl and e
-    vim.keymap.set('v', '<C-e>', ':Neotree filesystem reveal left<CR>', {})
-    vim.keymap.set('t', '<C-\\><C-n><C-e>', ':Neotree filesystem reveal left<CR>', { noremap = true, silent = true })
-    vim.keymap.set('i', '<Esc><C-e>', ':Neotree filesystem reveal left<CR>', {})
-  end
+    require("neo-tree").setup({})
+    vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal left<CR>", {}) -- <CR> immitates enter so we don't have to press enter after Ctrl and e
+    vim.keymap.set("v", "<C-e>", ":Neotree filesystem reveal left<CR>", {})
+    vim.keymap.set("t", "<C-e>", ":Neotree filesystem reveal left<CR>", { noremap = true, silent = true })
+    vim.keymap.set("i", "<C-e>", "<Esc>:Neotree filesystem reveal left<CR>", { noremap = true, silent = true })
+  end,
 }
