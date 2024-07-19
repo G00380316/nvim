f3e869c189eb80064a97204f9001fab77f553834
ENOCH ABIODUN <G00380316@atu.ie>
2024-07-19 11:30:28 +0100

    Small changes to Neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,15 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    require("neo-tree").setup({})
+    require("neo-tree").setup({
+      buffers = {
+          follow_current_file = {
+            enabled = true, -- This will find and focus the file in the active buffer every time
+            --              -- the current file is changed while the tree is open.
+            leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+          }
+      }
+   })
     vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal left<CR>", {}) -- <CR> immitates enter so we don't have to press enter after Ctrl and e
     vim.keymap.set("v", "<C-e>", ":Neotree filesystem reveal left<CR>", {})
     vim.keymap.set("t", "<C-e>", ":Neotree filesystem reveal left<CR>", { noremap = true, silent = true })
