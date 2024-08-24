7c7f31f835fea65e9fcf71d13c828f4438389247
Enoch Abiodun <ezea128@gmail.com>
2024-08-24 03:10:41 +0100

    Ye I think this is it for now

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -41,7 +41,7 @@ vim.g.maplocalleader = ","
 
 -- Reduce the timeout for mapped sequences
 vim.opt.timeoutlen = 200 -- You can adjust this to a lower value if needed
-vim.opt.ttimeoutlen = 0 -- Reduce this as well for faster response
+vim.opt.ttimeoutlen = 0  -- Reduce this as well for faster response
 
 vim.opt.swapfile = false
 vim.opt.updatetime = 1
@@ -66,9 +66,9 @@ local auto_save_group = vim.api.nvim_create_augroup("AutoSave", { clear = true }
 
 -- Auto-save on CursorHold
 vim.api.nvim_create_autocmd("CursorHold", {
-	group = auto_save_group,
-	pattern = "*",
-	command = "silent! write",
+  group = auto_save_group,
+  pattern = "*",
+  command = "silent! write",
 })
 
 -- Auto-Refresh Neo-tree on ModeChange
@@ -103,44 +103,44 @@ vim.api.nvim_create_autocmd("CursorHold", {
 --})
 
 -- Function to find the nearest directory containing package.json or .git
---local function find_project_root()
---	local path = vim.fn.expand("%:p:h")
+local function find_project_root()
+  local path = vim.fn.expand("%:p:h")
 
-	-- First, look for the nearest package.json
---	local package_json_dir = vim.fn.findfile("package.json", path .. ";")
---	if package_json_dir ~= "" then
---		return vim.fn.fnamemodify(package_json_dir, ":p:h")
---	end
+  -- First, look for the nearest package.json
+--  local package_json_dir = vim.fn.findfile("package.json", path .. ";")
+--  if package_json_dir ~= "" then
+--    return vim.fn.fnamemodify(package_json_dir, ":p:h")
+--  end
 
-	-- If no package.json is found, look for the nearest .git directory
---	local git_dir = vim.fn.finddir(".git", path .. ";")
---	if git_dir ~= "" then
-		-- Return the parent directory of the .git directory
---		return vim.fn.fnamemodify(git_dir, ":p:h:h")
---	end
+  -- If no package.json is found, look for the nearest .git directory
+  local git_dir = vim.fn.finddir(".git", path .. ";")
+  if git_dir ~= "" then
+    -- Return the parent directory of the .git directory
+    return vim.fn.fnamemodify(git_dir, ":p:h:h")
+  end
 
-	-- If neither is found, fall back to the current file's directory
---	return path
---end
+  -- If neither is found, fall back to the current file's directory
+  return path
+end
 
 -- Auto-change directory to the nearest package.json, .git's parent directory, or current file's directory
---vim.api.nvim_create_autocmd("BufLeave", {
---	pattern = "*",
---	callback = function()
---		local project_root = find_project_root()
---		if project_root then
---			vim.cmd("silent! cd " .. project_root)
---		end
---	end,
---})
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  callback = function()
+    local project_root = find_project_root()
+    if project_root then
+      vim.cmd("silent! cd " .. project_root)
+    end
+  end,
+})
 
 -- Open compiler
 vim.api.nvim_set_keymap("n", "<F6>", "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap("t", "<C-v>", "<C-\\><C-n>", { noremap = true }) -- For canceling float terminal proccess
-vim.api.nvim_set_keymap("n", "<C-s>", "<C-w>w", { noremap = true, silent = true }) -- Remaps the switch window in nvim to (ctrl and s)
-vim.api.nvim_set_keymap("i", "<C-s>", "<Esc><C-w>w", { noremap = true, silent = true }) -- Remaps the switch window in nvim to (ctrl and s)
-vim.api.nvim_set_keymap("v", "<C-s>", "<Esc><C-w>w", { noremap = true, silent = true }) -- Remaps the switch window in nvim to (ctrl and s)
+vim.api.nvim_set_keymap("t", "<C-v>", "<C-\\><C-n>", { noremap = true })                      -- For canceling float terminal proccess
+vim.api.nvim_set_keymap("n", "<C-s>", "<C-w>w", { noremap = true, silent = true })            -- Remaps the switch window in nvim to (ctrl and s)
+vim.api.nvim_set_keymap("i", "<C-s>", "<Esc><C-w>w", { noremap = true, silent = true })       -- Remaps the switch window in nvim to (ctrl and s)
+vim.api.nvim_set_keymap("v", "<C-s>", "<Esc><C-w>w", { noremap = true, silent = true })       -- Remaps the switch window in nvim to (ctrl and s)
 vim.api.nvim_set_keymap("t", "<C-s>", "<C-\\><C-n><C-w>w", { noremap = true, silent = true }) -- Remaps the switch window in nvim to (ctrl and s)
 
 -- Indent selected block of text to use this by using shift and then arrow key
@@ -162,8 +162,8 @@ vim.api.nvim_set_keymap("n", "<leader>x", '"+d', { noremap = true, silent = true
 vim.api.nvim_set_keymap("n", "<leader>d", ":nohlsearch<CR>", { noremap = true, silent = true })
 
 vim.keymap.set({ "n", "t", "i", "v" }, "<C-e>", function()
-	local dir = vim.fn.input("Directory: ", vim.fn.getcwd(), "dir")
-	vim.cmd("cd " .. dir)
+  local dir = vim.fn.input("Directory: ", vim.fn.getcwd(), "dir")
+  vim.cmd("cd " .. dir)
 end, { noremap = true, silent = true })
 
 -- Remap Shift + R to r
